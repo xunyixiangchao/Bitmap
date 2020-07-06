90cff0b4e0b979a2e8b4462e5f250e33e8a85722
lishuai <lishuai1@leyou.com>
2020-07-06 14:36:52 +0800

    添加磁盘缓存

--- app/src/main/java/com/retrofit/bitmap/MainActivity.java
+++ app/src/main/java/com/retrofit/bitmap/MainActivity.java
@@ -2,7 +2,9 @@ package com.retrofit.bitmap;
 
 import android.app.ActivityManager;
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 
 import java.io.IOException;
@@ -23,25 +25,55 @@ public class MainActivity extends AppCompatActivity {
         ActivityManager service = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
 //        int memory = service.getLargeMemoryClass() > 0 ? service.getLargeMemoryClass()
 //                                                       : service.getMemoryClass();
+
         int memory = service.getMemoryClass();
-        BitmapCache.getInstance().init(memory / 8);
+        String dir = getExternalCacheDir() + "bitmap";
+        //init方法最好写在application中
+        BitmapCache.getInstance().init(memory / 8, dir);
         InputStream open = null;
+        ImageView imageView = findViewById(R.id.image);
         try {
-            ImageView imageView = findViewById(R.id.image);
-            open = getResources().getAssets().open("guimie.jpg");
-//            Log.e("Bitmap",open.toString());
-            imageView.setImageBitmap(BitmapResize.resizeBitmapFromStream(open, 800, 600, true, null));
+            open = getResources().getAssets().open("big2.jpg");
         } catch (IOException e) {
             e.printStackTrace();
-        } finally {
-            if (open != null) {
-                try {
-                    open.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+        }
+//        imageView.setImageBitmap(BitmapResize.resizeBitmapFromStream(open, 800, 600, true, null));
+        String key = "big2";
+        //第一次
+        //内存缓存
+        Bitmap bitmap = BitmapCache.getInstance().getBitmapFromLru(key);
+        Log.e("main", "使用内存缓存" + bitmap);
+//        BitmapCache.getInstance().clearDisk();
+        if (bitmap == null) {
+            //复用缓存
+            Bitmap reusable = BitmapCache.getInstance().getReusableBitmap(1000, 1000, 1);
+            Log.e("main", "使用复用缓存" + reusable);
+            //磁盘缓存
+            bitmap = BitmapCache.getInstance().getBitmapFromDisk(key, reusable);
+            Log.e("main", "使用磁盘缓存" + bitmap);
+            if (bitmap == null) {
+                // 网络获取
+                bitmap = BitmapResize.resizeBitmapFromStream(open, 1000, 1000, true, reusable);
+                //放入内存
+                BitmapCache.getInstance().putBitmap2Lru(key, bitmap);
+                //放入磁盘
+                BitmapCache.getInstance().putBitmap2Disk(key, bitmap);
             }
         }
-
+        imageView.setImageBitmap(bitmap);
+        //第二次
+        //内存缓存
+        bitmap = BitmapCache.getInstance().getBitmapFromLru(key);
+        Log.e("main", "使用内存缓存2" + bitmap);
+        //复用缓存
+        Bitmap reusable = BitmapCache.getInstance().getReusableBitmap(160, 160, 1);
+        Log.e("main", "使用复用缓存2" + reusable);
+        //磁盘缓存
+        bitmap = BitmapCache.getInstance().getBitmapFromDisk(key, reusable);
+        Log.e("main", "使用磁盘缓存2" + bitmap);
+        imageView.setImageBitmap(bitmap);
+        BitmapCache.getInstance().clearDisk();
+        bitmap = BitmapCache.getInstance().getBitmapFromDisk(key, reusable);
+        Log.e("main", "使用磁盘缓存3" + bitmap);
     }
 }
